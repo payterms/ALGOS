6901c726c99019b8e3820b5afa0fefc755288f1f
Michael V. Gritsenko <payterms@mail.ru>
2020-04-04 19:46:00 +0300

    Initial commit

---
+++ src/Lesson2/Main.java
@@ -0,0 +1,12 @@
+package Lesson2;
+
+public class Main {
+    private static int ARR_SIZE = 1_000_000;
+
+    public static void main(String[] args) {
+        TestArray arr = new TestArray(ARR_SIZE);
+        for (int i = 0; i < ARR_SIZE; i++) {
+
+        }
+    }
+}
